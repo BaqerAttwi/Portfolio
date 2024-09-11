41ccd274a53a04c179a69b9f714620f1d06858e0
BaqerAttwi <mhmd.baqer.attwi@gmail.com>
2024-09-11 21:55:15 +0300

    Responsive fixed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,4 @@
 import { Container, Row, Col } from "react-bootstrap";
-import logo from "../assets/img/logo.svg";
 import navIcon1 from "../assets/img/nav-icon1.svg";
 import navIcon2 from "../assets/img/nav-icon2.svg";
 import navIcon3 from "../assets/img/nav-icon3.svg";
@@ -15,9 +14,11 @@ export const Footer = () => {
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="#"><img src={navIcon1} alt="Icon" /></a>
-              <a href="#"><img src={navIcon2} alt="Icon" /></a>
-              <a href="#"><img src={navIcon3} alt="Icon" /></a>
+            <a href="https://www.linkedin.com/in/mohammad-attwi-509b90307?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app 
+"><img src={navIcon1} alt="" /></a>
+
+                <a href="https://wa.me/+96181641596"><img src={navIcon2} alt="" /></a>
+                <a href="https://github.com/BaqerAttwi"><img src={navIcon3} alt="" /></a>
             </div>
             <p>Copyright 2024. All Rights Reserved</p>
           </Col>
